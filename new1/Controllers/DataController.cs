bc8c14c8ff06cafc77ce8770d549d7b1f410766c
Me-Hn <zzainkhan313@gmail.com>
2025-02-18 10:55:16 +0500

    Fetch Data

---
+++ new1/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using new1.Models;
+
+namespace new1.Controllers
+{
+    public class DataController : Controller
+    {
+        private ApplicationContext _context;
+
+       public DataController(ApplicationContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            //var q = from s in _context.employees select s;
+
+            //var n = from ft in _context.employees where ft.ID == 2 select ft;
+
+            //var n = from ft in _context.employees where ft.Name like ;
+
+
+
+
+
+            return View();
+        }
+    }
+}
